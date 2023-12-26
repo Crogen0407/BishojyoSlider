0791cf1399a8e9af295db3864424b787a81a1e1f
Crogen0407 <mymcoding0407@gmail.com>
2023-12-26 09:32:12 +0900

    feat : ProjectSettingValue Save... to the SO

--- Assets/Editor/BishojyoEditorData.cs
+++ Assets/Editor/BishojyoEditorData.cs
@@ -1,24 +1,24 @@
 ﻿using UnityEditor;
 using UnityEngine;
 
-public static class BishojyoEditorData
+[CreateAssetMenu(menuName = "BishojyoSlider/ProjectData", fileName = "ProjectData")]
+public class BishojyoEditorData : ScriptableObject
 {
-    public static int SliderCount { get; private set; }
-    public static float SliderValue { get; set; }
-    public static Vector2 screenSize = new Vector2(1920, 1080);
-    public static Rect windowRect = new Rect(new Vector2(0, 0), screenSize);
+    public string projectCallType;
+    public int sliderCount;
+    public float sliderValue;
+    public Vector2 screenSize = new Vector2(1920, 1080);
+    public Rect windowRect; 
     
-    public static float gap = 2;
-    public static float percentY = screenSize.y / screenSize.x;
-    public static float percentX = screenSize.x / screenSize.y;
-    public static int activePanelIndex;
-    
-    public static void Init()
+    public float gap = 2;
+    public float percentY;
+    public float percentX;
+    public int activePanelIndex;
+
+    [ContextMenu("Init")]
+    public  void Init()
     {
-        SliderCount = 10;
-        screenSize = new Vector2(1920, 1080);
         windowRect = new Rect(new Vector2(0, 0), screenSize);
-        gap = 2;                               
         percentY = screenSize.y / screenSize.x;
         percentX = screenSize.x / screenSize.y;
     }
--- Assets/Editor/BishojyoSliderPanel.cs
+++ Assets/Editor/BishojyoSliderPanel.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections.Generic;
 using System.Linq;
 using UnityEditor;
+using UnityEditorInternal;
 using UnityEngine;
 using UnityEngine.UIElements;
 
@@ -32,103 +33,130 @@ namespace Editor
         bool resize = false;
         Rect cursorChangeRect;
         private Vector2 timelineScrollVec;
+
+        private BishojyoEditorData _currentData;
+        
+        private List<GameObject> DD;
+
+        private string callName;        
         private void OnGUI()
         {
-            BishojyoEditorData.Init();
-            float gap = 25;
-            float mm = 50;
-            float aa = position.width > position.height ? position.height - mm  : position.width - mm;
-            Vector2 windowSize = new Vector2(position.width, position.height);
-            Vector2 sideWindowSize = new Vector2(windowSize.x / 5, windowSize.y*0.7f);
-            Rect mainScreenRect = new Rect(Vector2.right * sideWindowSize.x,
-                new Vector2(windowSize.x - sideWindowSize.x * 2, windowSize.y * 0.7f));
-            _currentActivePanelIndex = BishojyoEditorData.activePanelIndex;
-            Rect timelineRect = new Rect(new Vector2(0, windowSize.y * 0.7f),
-                new Vector2(windowSize.x, windowSize.y - windowSize.y * 0.7f));
-            
-            GUILayout.BeginArea(new Rect(Vector2.zero, new Vector2(sideWindowSize.x, mainScreenRect.size.y)), GUI.skin.window); //ProjectSetting
+            object selectionObject = UnityEditor.Selection.activeObject;
+            if(selectionObject != null && selectionObject.GetType() == (typeof(BishojyoEditorData)))
             {
-                GUILayout.BeginArea(new Rect(Vector2.zero, new Vector2(sideWindowSize.x, 25)), GUI.skin.box);
-                {
-                    GUI.TextField(new Rect(5, 0, 100, 20), "ProjectSetting", GUI.skin.label);
-                }
-                GUILayout.EndArea();
+                _currentData = selectionObject as BishojyoEditorData;
             }
-            GUILayout.EndArea();
-            GUILayout.BeginArea(new Rect(Vector2.right * (windowSize - sideWindowSize), new Vector2(sideWindowSize.x, mainScreenRect.size.y)), GUI.skin.window); //Hierarchy
-            {
-                GUILayout.BeginArea(new Rect(Vector2.zero, new Vector2(sideWindowSize.x, 25)), GUI.skin.box);
-                {
-                    GUI.TextField(new Rect(5, 0, 100, 20), "Hierarchy", GUI.skin.label);
-                }
-                GUILayout.EndArea();
-            }
-            GUILayout.EndArea();
-            GUILayout.BeginArea(mainScreenRect, GUI.skin.window); //MainScreen
-            {
-                #region MainScreen
-            
-                windowRect = new Rect(
-                    new Vector2(
-                        (mainScreenRect.size.x - _screenSize.x) / 2,
-                        (mainScreenRect.size.y - _screenSize.y) / 2), 
-                    _screenSize);
-                GUILayout.BeginArea(windowRect, GUI.skin.window);
-                {
-                    if (_currentActivePanelIndex != BishojyoEditorData.activePanelIndex)
-                    {
-                            
-                    }
-                    GUILayout.TextArea(BishojyoEditorData.activePanelIndex.ToString(), GUIStyle.none);
-                }
-                GUILayout.EndArea();
-                    
-                Event e = Event.current;
-                if (e.isScrollWheel)
-                {
-                    if (e.delta.y < 0)
-                    {
-                        _mulSize += 0.2f * Time.deltaTime;
-                    }
-                    else
-                    {
-                        _mulSize -= 0.2f * Time.deltaTime;
-                    }
-                }
-                _screenSize = new Vector2(aa, aa * BishojyoEditorData.percentY) * 1.5f * _mulSize;
-                GUI.TextField(new Rect(5, 0, 50, 20), "Scale", GUI.skin.label);
-                GUI.TextField(new Rect(mainScreenRect.size.x * 0.3f + 55, 0, 50, 20), _mulSize.ToString(), 4, GUI.skin.label);
 
-                _mulSize = GUI.HorizontalSlider(new Rect(45, 2.5f, mainScreenRect.size.x * 0.3f, 10), _mulSize, 0.2f, 3);
-                _mulSize = Mathf.Clamp(_mulSize, 0.2f, 3);
-                #endregion
-            }
-            GUILayout.EndArea();
-            GUILayout.BeginArea(timelineRect, GUI.skin.window); //Timeline
+            if (_currentData != null)
             {
-                #region UnderToolbar
-            
-                Vector2 panelSize = new Vector2(timelineRect.size.y * BishojyoEditorData.percentX, timelineRect.size.y) * 0.7f;
-                timelineScrollVec = GUILayout.BeginScrollView(timelineScrollVec, true, false, GUILayout.Width(timelineRect.size.x));
+                 float gap = 25;
+                float mm = 50;
+                float aa = position.width > position.height ? position.height - mm  : position.width - mm;
+                Vector2 windowSize = new Vector2(position.width, position.height);
+                Vector2 sideWindowSize = new Vector2(windowSize.x / 5, windowSize.y*0.7f);
+                Rect mainScreenRect = new Rect(Vector2.right * sideWindowSize.x,
+                    new Vector2(windowSize.x - sideWindowSize.x * 2, windowSize.y * 0.7f));
+                _currentActivePanelIndex = _currentData.activePanelIndex;
+                Rect timelineRect = new Rect(new Vector2(0, windowSize.y * 0.7f),
+                    new Vector2(windowSize.x, windowSize.y - windowSize.y * 0.7f));
+                
+                GUILayout.BeginArea(new Rect(Vector2.zero, new Vector2(sideWindowSize.x, mainScreenRect.size.y / 2)), GUI.skin.window); //ProjectSetting
                 {
-                    GUILayout.BeginHorizontal();
-
-                    for (int i = 0; i < BishojyoEditorData.SliderCount; i++)
+                    GUILayout.BeginArea(new Rect(Vector2.zero, new Vector2(sideWindowSize.x, 25)), GUI.skin.box);
                     {
-                        if (GUILayout.Button(i.ToString(), GUILayout.Width(panelSize.x), GUILayout.Height(panelSize.y)))
+                        GUI.TextField(new Rect(5, 0, 100, 20), "ProjectSetting", GUI.skin.label);
+                    }
+                    GUILayout.EndArea();
+                    //현재 작업중...
+                    _currentData.projectCallType = EditorGUILayout.DelayedTextField(_currentData.projectCallType);
+                
+                }     
+                GUILayout.EndArea();
+                GUILayout.BeginArea(new Rect(new Vector2(0, mainScreenRect.size.y / 2), new Vector2(sideWindowSize.x, mainScreenRect.size.y / 2)), GUI.skin.window); //Inspector
+                {
+                    GUILayout.BeginArea(new Rect(Vector2.zero, new Vector2(sideWindowSize.x, 25)), GUI.skin.box);
+                    {
+                        GUI.TextField(new Rect(5, 0, 100, 20), "Inspector", GUI.skin.label);
+                    }
+                    GUILayout.EndArea();
+                }     
+                GUILayout.EndArea();
+                GUILayout.BeginArea(new Rect(Vector2.right * (windowSize - sideWindowSize), new Vector2(sideWindowSize.x, mainScreenRect.size.y)), GUI.skin.window); //Hierarchy
+                {
+                    GUILayout.BeginArea(new Rect(Vector2.zero, new Vector2(sideWindowSize.x, 25)), GUI.skin.box);
+                    {
+                        GUI.TextField(new Rect(5, 0, 100, 20), "Hierarchy", GUI.skin.label);
+                    }
+                    GUILayout.EndArea();
+                }
+                GUILayout.EndArea();
+                GUILayout.BeginArea(mainScreenRect, GUI.skin.window); //MainScreen
+                {
+                    #region MainScreen
+                
+                    windowRect = new Rect(
+                        new Vector2(
+                            (mainScreenRect.size.x - _screenSize.x) / 2,
+                            (mainScreenRect.size.y - _screenSize.y) / 2), 
+                        _screenSize);
+                    GUILayout.BeginArea(windowRect, GUI.skin.window);
+                    {
+                        if (_currentActivePanelIndex != _currentData.activePanelIndex)
                         {
-                            BishojyoEditorData.activePanelIndex = i;
+                                
+                        }
+                        GUILayout.TextArea(_currentData.activePanelIndex.ToString(), GUIStyle.none);
+                    }
+                    GUILayout.EndArea();
+                        
+                    Event e = Event.current;
+                    if (e.isScrollWheel)
+                    {
+                        if (e.delta.y < 0)
+                        {
+                            _mulSize += 0.2f * Time.deltaTime;
+                        }
+                        else
+                        {
+                            _mulSize -= 0.2f * Time.deltaTime;
                         }
                     }
-                    GUILayout.EndHorizontal();
+                    _screenSize = new Vector2(aa, aa * _currentData.percentY) * 1.5f * _mulSize;
+                    GUI.TextField(new Rect(5, 0, 50, 20), "Scale", GUI.skin.label);
+                    GUI.TextField(new Rect(mainScreenRect.size.x * 0.3f + 55, 0, 50, 20), _mulSize.ToString(), 4, GUI.skin.label);
+                
+                    _mulSize = GUI.HorizontalSlider(new Rect(45, 2.5f, mainScreenRect.size.x * 0.3f, 10), _mulSize, 0.2f, 3);
+                    _mulSize = Mathf.Clamp(_mulSize, 0.2f, 3);
+                    #endregion
                 }
-                GUILayout.EndScrollView();
-
-                #endregion
+                GUILayout.EndArea();
+                GUILayout.BeginArea(timelineRect, GUI.skin.window); //Timeline
+                {
+                    #region UnderToolbar
+                
+                    Vector2 panelSize = new Vector2(timelineRect.size.y * _currentData.percentX, timelineRect.size.y) * 0.7f;
+                    timelineScrollVec = GUILayout.BeginScrollView(timelineScrollVec, false, false, GUILayout.Width(timelineRect.size.x));
+                    {
+                        GUILayout.BeginHorizontal();
+                
+                        for (int i = 0; i < _currentData.sliderCount; i++)
+                        {
+                            if (GUILayout.Button(i.ToString(), GUILayout.Width(panelSize.x), GUILayout.Height(panelSize.y)))
+                            {
+                                _currentData.activePanelIndex = i;
+                            }
+                        }
+                        GUILayout.EndHorizontal();
+                    }
+                    GUILayout.EndScrollView();
+                
+                    #endregion
+                }
+                GUILayout.EndArea();
+                
+                Repaint();
             }
-            GUILayout.EndArea();
-            
-            Repaint();
+           
         }
     }
 }
